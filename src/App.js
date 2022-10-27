fde9b420ffd6c66573357164d531e64c1b3ba04d
Gauri Jagdale <104825003+Gauri319@users.noreply.github.com>
2022-10-27 16:48:11 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import React, { useState, useEffect } from "react";
+import "./styles.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Signup from "./Components/Signup";
+import Todos from "./Components/Todos";
+import { auth } from "./Config/firebaseInitialise";
+
+export default function App() {
+  const [user, setUser] = useState(null);
+  useEffect(() => {
+    auth.onAuthStateChanged((user) => {
+      if (user) {
+        setUser(user);
+        console.log(user);
+      } else {
+        setUser(null);
+        console.log("user not found");
+      }
+    });
+  }, []);
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/todos" exact element={<Todos user={user} />} />
+          <Route path="/" exact element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
